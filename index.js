232971cbaff3a55163b23524df90755831b636a9
Raro Macias <ggmacias@gmail.com>
2022-09-21 17:38:21 -0400

    fixed

--- index.js
+++ index.js
@@ -7,9 +7,19 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, '../deployment/client/index.html')))
+// app.use('/', express.static(path.join(__dirname, '/client/index.html')))
 
-app.use(express.static(path.join(__dirname, '../deployment/client')))
+
+// app.use(express.static(path.join(__dirname, '/client')))
+
+app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '/client/index.html'))
+    })
+
+app.get('/css', (req, res) => {
+        res.sendFile(path.join(__dirname, '/client/index.css'))
+    })
+    
 
 app.listen(port, () => {
     console.log('Docked at port ' + port)
